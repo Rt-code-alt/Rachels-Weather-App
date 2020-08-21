ef7ce2a8fb67eba1d07823ed9ec80b02f6f1494c
Rt-code-alt <68718792+Rt-code-alt@users.noreply.github.com>
2020-08-21 14:15:21 +1000

    Added working file from week 5 (She codes course)

---
+++ src/index.js
@@ -0,0 +1,146 @@
+function displayWeather(response) {
+  //let weatherIcon = document.querySelector("#todayWeatherIcon");
+  //weatherIcon.innerHTML = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+
+  let currentWeatherIcon = document.querySelector("#current-weather-icon");
+  currentWeatherIcon.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentWeatherIcon.setAttribute("alt", response.data.weather[0].description);
+
+  let temperature = Math.round(response.data.main.temp);
+  let degrees = document.querySelector(".degrees");
+  degrees.innerHTML = `${temperature}`;
+
+  let description = document.querySelector("#description");
+  let updateDescription = response.data.weather[0].main;
+  description.innerHTML = ` ${updateDescription} `;
+
+  let tempMin = document.querySelector("#temperatureTodayMin");
+  tempMin.innerHTML = Math.round(response.data.main.temp_min);
+
+  let tempMax = document.querySelector("#temperatureTodayMax");
+  tempMax.innerHTML = Math.round(response.data.main.temp_max);
+
+  let windSpeed = document.querySelector("#windSpeedToday");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+  document.querySelector(
+    "#humidityToday"
+  ).innerHTML = `${response.data.main.humidity}`;
+
+  let country = document.querySelector("#country");
+  country.innerHTML = `${response.data.sys.country}`;
+
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = response.data.name;
+
+  // to find any other variable I want to show console.log(response.data);
+}
+
+// location search engine
+
+function search(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#form-control");
+  let cityChosen = `${cityInput.value}`;
+  let h2 = document.querySelector("#city-name");
+  h2.innerHTML = cityChosen;
+  let apiKey = "9323723efb08066b7b3d194977c6162d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityChosen}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let form = document.querySelector("#location-search");
+form.addEventListener("submit", search);
+
+// current location
+
+function showPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+  let apiKey = "9323723efb08066b7b3d194977c6162d";
+  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function localPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", localPosition);
+
+// celsius > fareinheit conversion
+function changeToFarenheit(event) {
+  event.preventDefault();
+  let farenhenheitDegrees = document.querySelector(".degrees");
+  let fTemp = Math.round((farenhenheitDegrees.innerHTML * 9) / 5 + 32);
+  farenhenheitDegrees.innerHTML = `${fTemp}`;
+}
+
+let farenheit = document.querySelector("#fahrenheit-link");
+farenheit.addEventListener("click", changeToFarenheit);
+
+// farenheit > celsius conversion
+function changeToCelsius(event) {
+  event.preventDefault();
+  let celsiusDegrees = document.querySelector(".degrees");
+  let cTemp = Math.round(((celsiusDegrees.innerHTML - 32) * 5) / 9);
+  celsiusDegrees.innerHTML = `${cTemp}`;
+}
+
+let celsius = document.querySelector("#celsius-link");
+celsius.addEventListener("click", changeToCelsius);
+
+// time and date
+
+let now = new Date();
+let currentTime = document.querySelector(".time-and-date");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let date = now.getDate();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let tt = "";
+if (hours >= 12) {
+  tt = "PM";
+} else {
+  tt = "AM";
+}
+
+minutes = minutes < 10 ? "0" + minutes : minutes;
+currentTime.innerHTML = `${day} ${date}th ${month} ${hours}:${minutes} ${tt}`;
